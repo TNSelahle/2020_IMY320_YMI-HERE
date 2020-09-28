d1f2cca359857df9b7df08a7d85e973f52f9f1fe
Ruslynn Appana <38566658+Ryperior@users.noreply.github.com>
2020-09-28 15:48:54 +0200

    Update playvideo.js

--- public/hub/playvideo.js
+++ public/hub/playvideo.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
-    var figure = $(".video1").hover( hoverVideo, hideVideo );
+    /* var figure = $(".video1").hover( hoverVideo, hideVideo );
     var figure = $(".video2").hover( hoverVideo, hideVideo );
     var figure = $(".video3").hover( hoverVideo, hideVideo );
     var figure = $(".video4").hover( hoverVideo, hideVideo );
-    var figure = $(".video5").hover( hoverVideo, hideVideo );
+    var figure = $(".video5").hover( hoverVideo, hideVideo ); */
     /* function hoverVideo(e) {  
         $('video', this).get(0).play(); 
     }
