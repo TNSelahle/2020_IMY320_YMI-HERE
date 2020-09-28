b63c6d64afc684538ac588583dabb1c93d0eb2cb
Ruslynn Appana <38566658+Ryperior@users.noreply.github.com>
2020-09-28 15:10:29 +0200

    Add files via upload

--- playvideo.js
+++ playvideo.js
@@ -4,14 +4,14 @@ $(document).ready(function(){
     var figure = $(".video3").hover( hoverVideo, hideVideo );
     var figure = $(".video4").hover( hoverVideo, hideVideo );
     var figure = $(".video5").hover( hoverVideo, hideVideo );
-    function hoverVideo(e) {  
+    /* function hoverVideo(e) {  
         $('video', this).get(0).play(); 
     }
     
     function hideVideo(e) {
         $('video', this).get(0).pause(); 
     }
-    
+     */
     });
 
 (function () {
